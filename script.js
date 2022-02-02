8d3716ee5ce8caa4d0445b034ec1bf37355a2dcd
clopez333 <97922177+clopez333@users.noreply.github.com>
2022-02-02 11:36:12 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const Randomize = () => {
+    let messages = ['Today I will do what others won\'t, so tomorrow I can accomplish what others can\'t - Jerry Rice',
+        'Precision beats power and timing beats speed - Conor McGregor',
+        'You have time - Gary Vaynerchuk'
+    ];
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    let randomMsg = Math.floor(Math.random() * messages.length);
+    let randomNum = Math.floor(Math.random() * numbers.length);
+
+    let output = [];
+    output.push('Your motivational quote is: ' + messages[randomMsg] + ' and your number is: ' + numbers[randomNum]);
+    return output
+};
+
+console.log(Randomize());
